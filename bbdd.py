9a9fe78dd4fb75d977f1d7ad0606f0c2f65db58b
Daniel Suárez <dwsuarez98@gmail.com>
2022-06-06 21:15:38 -0300

    modulo agregado

---
+++ bbdd.py
@@ -0,0 +1,28 @@
+import sqlite3
+MI_BASE = 'database.db'
+
+def run_query(query, parameters = ()):
+        with sqlite3.connect(MI_BASE) as conn:
+            cursor = conn.cursor()
+            result = cursor.execute(query, parameters)
+            conn.commit()
+        return result
+
+
+def conexionBBDD():
+    miConexion = sqlite3.connect('database.db')
+    miCursor = miConexion.cursor()
+
+    try: 
+        miCursor.execute('''
+            CREATE TABLE tipo(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre VARCHAR(50),
+            altura FLOAT NOT NULL,
+            peso INT NOT NULL,
+            IMC FLOAT)
+            ''')
+        print('BASE DE DATOS creada exitosamente')
+    except:
+        print('Conexión exitosa con la base de datos')
+
